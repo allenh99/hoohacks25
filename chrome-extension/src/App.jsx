9e0deceb7ac99849c08f705e1d65c674c1c33b6a
evanzhang <fym4cf@virginia.edu>
2025-03-30 02:30:09 -0400

    end of day 1

--- chrome-extension/src/App.jsx
+++ chrome-extension/src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [weight, setWeight] = useState("light");
   const [openDropdowns, setOpenDropdowns] = useState([]);
+  const [submitted, setSubmitted] = useState(false);
 
   const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-  const word = "Result:";
   const title = "Facts Analyzer";
 
   useEffect(() => {
@@ -40,35 +40,36 @@ function App() {
     return () => clearInterval(interval);
   }, []);
 
-  useEffect(() => {
-    if (!highlighted) return;
+  // useEffect(() => {
+  //   if (!highlighted) return;
 
-    const analyze = async () => {
-      setLoading(true);
-      // await sleep(5000); // for testing loading animation
+  const analyze = async () => {
+    setSubmitted(true);
+    setLoading(true);
+    await sleep(5000); // for testing loading animation
 
-      try {
-        const res = await fetch("http://127.0.0.1:8000/api/analyze/", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ 
-            text: highlighted,
-            weight: weight
-          })
-        });
-        const data = await res.json();
-        console.log(data);
-        //setResult(JSON.stringify(data, null, 2));
-        setResult(data || "No response");
-      } catch (err) {
-        setResult("Error fetching from backend.");
-        //console.log("ERROR:", err);
-      }
-      setLoading(false);
-    };
+    try {
+      const res = await fetch("http://127.0.0.1:8000/api/analyze/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ 
+          text: highlighted,
+          weight: weight
+        })
+      });
+      const data = await res.json();
+      console.log(data);
+      //setResult(JSON.stringify(data, null, 2));
+      setResult(data || "No response");
+    } catch (err) {
+      setResult("Error fetching from backend.");
+      //console.log("ERROR:", err);
+    }
+    setLoading(false);
+  };
 
-    analyze(); //call fetch function
-  }, [highlighted]);
+  //analyze(); //call fetch function
+  // }, [highlighted]);
 
   useEffect(() => {
     if (result?.ratings?.length) {
@@ -116,43 +117,76 @@ function App() {
         </div>
       </div>
 
+      {!submitted && (
+        <button
+          onClick={analyze}
+          className="bg-blue-600 text-white px-4 py-2 rounded-md shadow hover:bg-blue-700 transition duration-200 w-full font-medium text-sm mb-4"
+        >
+          Analyze
+        </button>
+      )}
+
       {loading && <LoadingDots />}
 
       {result && !loading &&(
-        <div className="grid grid-cols-1 gap-4 mt-4">
-          {result.ratings.map((rating, i) => (
-            <div
-              key={i}
-              className="rounded-xl bg-white shadow-md p-4 border border-gray-200 text-sm"
-            >
-              <h3 className="text-blue-600 font-semibold mb-2">Source {i + 1}</h3>
+        <div className="mt-4">      
+          Results:  
+          <div className="flex flex-wrap items-center justify-center bg-blue-100 border border-blue-300 text-blue-800 p-3 rounded-md mb-4 text-sm font-semibold">
+            {result.label.split("").map((char, i) => (
+              <span
+                key={i}
+                className="inline-block animate-bounce-loop"
+                style={{ animationDelay: `${i * 100}ms` }}
+              >
+                {char}
+              </span>
+            ))}
+          </div>
 
-              <p className="mb-1">
-                <span className="font-medium text-gray-600">Rating:</span>{" "}
-                {rating}
-              </p>
+          <div className="grid grid-cols-1 gap-4">
+            {result.ratings.map((rating, i) => (
+              <div
+                key={i}
+                className="rounded-xl bg-white shadow-md p-4 border border-gray-200 text-sm"
+              >
+                <div className="flex items-center justify-between mb-2">
+                  <h3 className="text-blue-600 font-semibold underline">
+                    <a
+                      href={result.sources[i]}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Source {i + 1}
+                    </a>
+                  </h3>
+                  <button onClick={() => toggleDropdown(i)}>
+                    {openDropdowns[i] ? "Less": "More"}
+                  </button>
+                </div>
 
-              <p className="text-blue-500 underline break-all">
-                <a
-                  href={result.sources[i]}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {result.sources[i]}
-                </a>
-              </p>
-
-              <button onClick={() => toggleDropdown(i)}>
-                {openDropdowns[i] ? "Hide": "Show"}
-              </button>
-
-              {openDropdowns[i] && (<p className="mb-1">
-                <span className="font-medium text-gray-600">Analysis:</span>{" "}
-                  {result.analysis[i]}
+                <p className="mb-1">
+                  <span className="font-medium text-gray-600">Rating:</span>{" "}
+                  {rating}/10
                 </p>
-              )}
-            </div>
-          ))}
+
+                {/* <p className="text-blue-500 underline break-all">
+                  <a
+                    href={result.sources[i]}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Link {i + 1}
+                  </a>
+                </p> */}
+
+                {openDropdowns[i] && (<p className="mb-1">
+                  <span className="font-medium text-gray-600">Analysis:</span>{" "}
+                    {result.analysis[i]}
+                  </p>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </div>
